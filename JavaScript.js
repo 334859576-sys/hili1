3780a0aebb6f8cad23a59a2116ca152d5afdd9a5
334859576-sys <334859576@ohel-shem.com>
2025-11-11 19:32:36 +0200

    first upload

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+﻿alert("heli");
+var num1 = parseInt(prompt("enter number"));
+var num2 = parseInt(prompt("enter number"));
+var result1 = num1 + num2
+var result2 = num1 - num2
+var result3 = num1 / num2
+var result4 = num1 * num2
+document.write(" result = ", result1);
+document.write(" result = ", result2);
+document.write(" result = ", result3);
+document.write(" result = ", result4);
